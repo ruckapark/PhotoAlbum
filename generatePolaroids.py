93207e07d988e754a11352df8e57f90d5884faeb
George Ruck <georgeadruck@gmail.com>
2023-10-03 08:45:47 +0200

    Working final polaroid generation - non object orientated code

--- generatePolaroids.py
+++ generatePolaroids.py
@@ -7,13 +7,42 @@ Created on Mon Oct  2 22:25:14 2023
 
 
 import os
+import numpy as np
 import cv2
 
-def add_image(base,image,pos):
+def find_mindim(images):
     
-    pos 0,1,2,3
+    #for square images
+    mindim = 1000000
+    for key in images:
+        dim = images[key].shape[0]
+        if dim < mindim: 
+            mindim = dim    
     
-    add to base image
+    return mindim
+
+def find_basedims(images,ratio = 4.6):
+    
+    mindim = find_mindim(images)
+    height,width = int(mindim * (15/ratio)),int(mindim * (10/ratio))
+    return mindim,height,width
+
+def add_image(images,num):
+    
+    mindim,height,width = find_basedims(images)
+    for l in images: images[l] = cv2.resize(images[l],(mindim,mindim)) #resize all images
+    base = np.zeros((height,width,3), np.uint8)
+    base.fill(255)
+    
+    #topleft corner coordinates
+    x = {0:int((0.2/10)*width),1:int((5.2/10)*width)}
+    y = {0:int((1/15)*height),1:int((8.5/15)*height)}
+    for i,l in enumerate([*images]):
+        row,col = i//2,i%2
+        start_x,start_y = x[col],y[row]
+        base[:, start_x:start_x+mindim][start_y:start_y+mindim] = images[l]
+    
+    return base                     
     
 
 if __name__ == '__main__':
@@ -22,13 +51,13 @@ if __name__ == '__main__':
     files = [f for f in files if 'Album' in f]
     
     pages = set([int(f.split('Album')[-1].split('.')[0][:-1]) for f in files])
+    images = {}
     
-    for i in range(max(baseimage)):
+    for i in range(max(pages)):
         
-        baseimage = ?? 10*15 blank white image
-        
-        for j,l in enumerate(['a','b','c','d']):
+        for l in ['a','b','c','d']:
             
-            baseimage = add_image(baseimage,cv2.readimage('Album{}{}.jpg'.format(i+1,l)),j)
+            images.update({l:cv2.imread('Album{}{}.jpg'.format(i+1,l))})
             
-        baseimage.write(r'polaroids/Album{}.jpg'.format(i+1))
+        baseimage = add_image(images,i)
+        cv2.imwrite(r'polaroids/Album{}.jpg'.format(i+1),baseimage)
